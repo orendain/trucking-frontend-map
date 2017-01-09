a588b3ae57c0cb8bfe01750a2c958334c1a7c053
Edgar Orendain <edgar@orendainx.com>
2017-01-09 12:41:00 -0800

    Added files to git

---
+++ src/main/scala/com/hortonworks/orendainx/trucking/frontend/map/Map.scala
@@ -0,0 +1,21 @@
+package com.hortonworks.orendainx.trucking.frontend.map
+
+import angulate2.std._
+import angulate2.platformBrowser.BrowserModule
+
+@NgModule(
+  imports = @@[BrowserModule],
+  declarations = @@[MapComponent],
+  bootstrap = @@[MapComponent]
+)
+class MapModule {
+
+}
+
+@Component(
+  selector = "my-app",
+  template = "<h1>Hello Angular!<h1>"
+)
+class MapComponent {
+
+}
